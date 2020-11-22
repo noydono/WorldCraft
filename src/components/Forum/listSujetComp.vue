66fd0c16ed5af09f88ec0934bac9c9c2b0c00657
noydono <th.gabillard@laposte.net>
2020-11-22 06:41:49 +0100

    add pagination listSujet/:id bug refresh

--- src/components/Forum/listSujetComp.vue
+++ src/components/Forum/listSujetComp.vue
@@ -1,6 +1,5 @@
 <template>
   <v-row class="mt-12">
-    
     <v-card class="mx-auto" v-if="sujet.length != 0">
       <v-list
         :disabled="disabled"
@@ -14,12 +13,14 @@
         :nav="nav"
         :avatar="avatar"
         :rounded="rounded"
+        max-width="900"
+        width="800"
       >
         <v-subheader>Sujet</v-subheader>
         <v-list-item-group v-model="item" color="primary">
           <v-list-item
             :to="subItem.link"
-            v-for="subItem in sujet"
+            v-for="subItem in cloneItems"
             :key="subItem._id"
             @click="$router.push('/Article/12313661')"
           >
@@ -48,15 +49,30 @@
                 <div class="subtitle-2 grey--text">26 juin</div>
               </v-list-item-title>
               <v-list-item-avatar class="mr-5">
-                <v-img aspect-ratio="2" src="https://cdn.vuetifyjs.com/images/lists/5.jpg"></v-img>
+                <v-img
+                  aspect-ratio="2"
+                  src="https://cdn.vuetifyjs.com/images/lists/5.jpg"
+                ></v-img>
               </v-list-item-avatar>
             </div>
           </v-list-item>
+          <v-pagination
+            v-if="pagination.total >= pagination.rowsPerPage"
+            v-model="pagination.page"
+            :length="pages"
+            circle
+            color="light-green darken-1"
+            class="pb-5"
+          ></v-pagination>
         </v-list-item-group>
       </v-list>
     </v-card>
-    <v-card class="mx-auto" v-if="sujet.length == 0">
-        <h2>Il n'y a pas de sujet dans cette categorie</h2>
+    <v-card class="mx-auto text-center" v-if="sujet.length == 0">
+      <h2 class="py-5 px-5">oups !!!</h2>
+      <p class="px-5">
+        il n'y a pas encore de sujet soit le premier a en crée un.
+      </p>
+      <v-btn block> Crée un sujet </v-btn>
     </v-card>
   </v-row>
 </template>
@@ -64,7 +80,8 @@
 import { mapActions, mapState } from "vuex";
 
 export default {
-  data: () => ({
+  data() {
+    return {
       item: 5,
       disabled: false,
       dense: false,
@@ -78,18 +95,34 @@ export default {
       nav: false,
       avatar: true,
       rounded: false,
-    }),
+      pagination: {
+        page: 1,
+        total: 0,
+        rowsPerPage: 10,
+      },
+    };
+  },
+  mounted() {
+    this.pagination.total = this.sujet.length;
+  },
   created() {
     this.getSujet(this.$route.params.id);
-    
-
   },
   methods: {
-    ...mapActions(["getSujet"])
+    ...mapActions(["getSujet"]),
   },
   computed: {
-      
     ...mapState(["sujet"]),
+    pages() {
+      return Math.ceil(this.pagination.total / this.pagination.rowsPerPage);
+    },
+    cloneItems() {
+      var clone = JSON.parse(JSON.stringify(this.sujet));
+      var startFrom =
+        this.pagination.page * this.pagination.rowsPerPage -
+        this.pagination.rowsPerPage;
+      return clone.splice(startFrom, this.pagination.rowsPerPage);
+    },
   },
 };
 </script>
