568167a66f2f0ea61d7bfdd33125eb96e02d52cb
noydono <th.gabillard@laposte.net>
2021-01-25 00:14:36 +0100

    send mail send an email to registration and coverage >90%

--- test/test.js
+++ test/test.js
@@ -13,7 +13,6 @@ describe("App", () => {
   it("Should exists", () => {
     expect(app).to.be.a("function");
   });
-
   it("GET / should return 200 and message", (done) => {
     chai
       .request(app)
@@ -29,28 +28,30 @@ describe("App", () => {
   });
 });
 
+
+
 describe("User registration", () => {
   let new_user = {};
 
   beforeEach(function (done) {
-    new_user= {
+    new_user = {
       username: faker.name.firstName(),
-      email: faker.internet.email(),
-      password: faker.internet.password()+"1M*",
-    }
+      email: "noy.donoo@toto.toto",
+      password: faker.internet.password() + "1M*",
+    };
     done();
   });
-  
-  afterEach(function(done) {
-    UserModel.findOneAndDelete({username:new_user.username},(err,user)=>{
-      if(err){
-        console.log("LogTest_UserRegistration: erreur de suppresion d'utilisateur");
-      }else {
-        console.log("LogTest_UserRegistration: utilisateur suprimé");
-        done()
+
+  afterEach(function (done) {
+    UserModel.findOneAndDelete({ username: new_user.username }, (err, user) => {
+      if (err) {
+        console.log(
+          "LogTest_UserRegistration: "+ err);
+      } else {
+        done();
       }
-    })
-  })
+    });
+  });
 
   it("Should return 201 and confirmation for valid input", (done) => {
     chai
@@ -63,16 +64,53 @@ describe("User registration", () => {
         expect(res.body.errors.length).to.be.equal(0);
         expect(res.body.user._id).to.exist;
         expect(res.body.user.created_at).to.exist;
-        bcrypt.compare(new_user.password,res.body.user.password,(err,result)=>{
-          if(err){
-            console.log("LogTest_UserRegistration: erreur de compare de password : " + err)
-          }
-          expect(result).to.be.equal(true)
-        })
-        console.log(res.body.tokens);
-        expect(res.body.tokens).to.exist;
         done();
-        
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  });
+
+  it("Should return 201 and confirmation if the password is hashed", (done) => {
+    chai
+      .request(app)
+      .post("/user/register")
+      .send(new_user)
+      .then((res) => {
+        expect(res).to.have.status(201);
+        expect(res.body.message).to.be.equal("User created!");
+        expect(res.body.errors.length).to.be.equal(0);
+        bcrypt.compare(
+          new_user.password,
+          res.body.user.password,
+          (err, result) => {
+            if (err) {
+              console.log(
+                "LogTest_UserRegistration: erreur de compare de password : " +
+                  err
+              );
+            }
+            expect(result).to.be.equal(true);
+            done();
+          }
+        );
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  });
+
+  it("Should return 201 and confirmation if the jwt is persisted", (done) => {
+    chai
+      .request(app)
+      .post("/user/register")
+      .send(new_user)
+      .then((res) => {
+        expect(res).to.have.status(201);
+        expect(res.body.message).to.be.equal("User created!");
+        expect(res.body.errors.length).to.be.equal(0);
+        expect(res.body.user.tokens.length).to.be.equal(1);
+        done();
       })
       .catch((err) => {
         console.log(err.message);
@@ -148,54 +186,15 @@ describe("User registration", () => {
       });
   });
 
-  
-});
-
-describe("User Update",() =>{
-
-  const old_user = {
-    username: faker.name.firstName(),
-    email: faker.internet.email(),
-    password: faker.internet.password()+"1M*",
-  };
-  let new_user = {}
-
-  beforeEach(function (done) {
-    UserModel.create(old_user,(err,user)=>{
-      if(err){
-        console.log("LogTest_UserUpdate: erreur de creation d'utilisateur"+ err);
-      }else {
-        console.log("LogTest_UserUpdate: utilisateur"+ user.username +" créé");
-        new_user._id = user._id
-        done()
-      }
-    })
-  });
-
-  afterEach(function(done) {
-    UserModel.findOneAndDelete({username:new_user.username},(err)=>{
-      if(err){
-        console.log("LogTest_UserUpdate: erreur de suppresion d'utilisateur");
-      }else {
-        console.log("LogTest_UserUpdate: utilisateur suprimé");
-        done()
-      }
-    })
-  })
-
-  it("Should return 200 and message with the data is valid",(done)=>{
-    new_user.username = faker.name.firstName();
-    new_user.email = faker.internet.email();
-    new_user.password = faker.internet.password();
+  it("Should send email if the persite is done", (done) => {
     chai
       .request(app)
-      .put("/user/update/"+ new_user._id)
+      .post("/user/register")
       .send(new_user)
       .then((res) => {
-        expect(res).to.have.status(200);
-        expect(res.body.message).to.be.equal("update successful!");
+        expect(res).to.have.status(201);
+        expect(res.body.emailVerification).to.be.equal("email sent successfully!");
         expect(res.body.errors.length).to.be.equal(0);
-        expect(res.body.user.username).to.be.equal(new_user.username);
         done();
       })
       .catch((err) => {
@@ -203,41 +202,56 @@ describe("User Update",() =>{
       });
   });
 
-  
-  
-})
+});
+   
+// describe("User Update", () => {
+//   const old_user = {
+//     username: faker.name.firstName(),
+//     email: faker.internet.email(),
+//     password: faker.internet.password() + "1M*",
+//   };
+//   let new_user = {};
 
-// describe("User delete", () => {
-//   let new_user = {}
 //   beforeEach(function (done) {
-//     new_user = {
-//       _id:faker.internet.password(),
-//       username: faker.name.firstName(),
-//       email: faker.internet.email(),
-//       password: +"1M*m",
-//     };
-//     chai
-//       .request(app)
-//       .post("/user/register")
-//       .send(new_user)
-//       .then((res) => {
-//         done();
-//       })
-//       .catch((err) => {
-//         console.log(err.message);
-//       });
-//     });
-//   it("Should return 200 and message if user deleted", (done) => {
-//     chai
-//       .request(app)
-//       .delete(`/user/delete/${new_user._id}`)
-//       .send(new_user)
-//       .then((res) => {
-//         expect(res).to.have.status(201);
-//         expect(res.body.message).to.be.equal(
-//           "User " + req.body.user.username + " is deleted"
+//     UserModel.create(old_user, (err, user) => {
+//       if (err) {
+//         console.log(
+//           "LogTest_UserUpdate: erreur de creation d'utilisateur" + err
 //         );
+//       } else {
+//         console.log(
+//           "LogTest_UserUpdate: utilisateur" + user.username + " créé"
+//         );
+//         new_user._id = user._id;
+//         done();
+//       }
+//     });
+//   });
+
+//   afterEach(function (done) {
+//     UserModel.findOneAndDelete({ username: new_user.username }, (err) => {
+//       if (err) {
+//         console.log("LogTest_UserUpdate: erreur de suppresion d'utilisateur");
+//       } else {
+//         console.log("LogTest_UserUpdate: utilisateur suprimé");
+//         done();
+//       }
+//     });
+//   });
+
+//   it("Should return 200 and message with the data is valid", (done) => {
+//     new_user.username = faker.name.firstName();
+//     new_user.email = faker.internet.email();
+//     new_user.password = faker.internet.password();
+//     chai
+//       .request(app)
+//       .put("/user/update/" + new_user._id)
+//       .send(new_user)
+//       .then((res) => {
+//         expect(res).to.have.status(200);
+//         expect(res.body.message).to.be.equal("update successful!");
 //         expect(res.body.errors.length).to.be.equal(0);
+//         expect(res.body.user.username).to.be.equal(new_user.username);
 //         done();
 //       })
 //       .catch((err) => {
